fa426e226dd0fbf774708aa3eeffefd99c91f7d5
LucianoFrias <lucianofrias1@hotmail.com>
2024-03-29 20:43:41 -0300

    Added search box functionality and search filters

--- src/main/java/org/stockfound/stockfoundinventorysoftware/controllers/MainViewController.java
+++ src/main/java/org/stockfound/stockfoundinventorysoftware/controllers/MainViewController.java
@@ -1,16 +1,18 @@
 package org.stockfound.stockfoundinventorysoftware.controllers;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.stockfound.stockfoundinventorysoftware.entities.Item;
@@ -26,6 +28,10 @@ public class MainViewController implements Initializable {
     private final ItemService itemService;
 
     // FXML
+    @FXML
+    private TextField itemSearchBar;
+    @FXML
+    private ChoiceBox itemSearchFilters;
     @FXML
     private TableView<Item> itemTableView;
 
@@ -76,6 +82,8 @@ public class MainViewController implements Initializable {
         Stage addItemPopUpStage = new Stage();
         addItemPopUpStage.initModality(Modality.APPLICATION_MODAL);
         addItemPopUpStage.setTitle("Add Item");
+        addItemPopUpStage.getIcons().add(new Image(
+                String.valueOf(getClass().getResource("/org/stockfound/stockfoundinventorysoftware/stockfound-logo.jpg"))));
         addItemPopUpStage.setScene(new Scene(root));
 
 
@@ -99,6 +107,8 @@ public class MainViewController implements Initializable {
         Stage deleteItemPopupStage = new Stage();
         deleteItemPopupStage.initModality(Modality.APPLICATION_MODAL);
         deleteItemPopupStage.setTitle("Delete Item");
+        deleteItemPopupStage.getIcons().add(new Image(
+                String.valueOf(getClass().getResource("/org/stockfound/stockfoundinventorysoftware/stockfound-logo.jpg"))));
         deleteItemPopupStage.setScene(new Scene(root));
 
 
@@ -120,6 +130,8 @@ public class MainViewController implements Initializable {
         Stage editItemPopupStage = new Stage();
         editItemPopupStage.initModality(Modality.APPLICATION_MODAL);
         editItemPopupStage.setTitle("Edit Item");
+        editItemPopupStage.getIcons().add(new Image(
+                String.valueOf(getClass().getResource("/org/stockfound/stockfoundinventorysoftware/stockfound-logo.jpg"))));
         editItemPopupStage.setScene(new Scene(root));
 
 
@@ -133,12 +145,87 @@ public class MainViewController implements Initializable {
         itemTableView.refresh();
     }
 
+    public void filterData(){
+        FilteredList<Item> filteredItems = new FilteredList<Item>(itemService.getAllItems(), b -> true);
+
+        itemSearchBar.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredItems.setPredicate(item -> {
+
+                String lowerCaseFilter = newValue.toLowerCase();
+
+                if (itemSearchFilters.getSelectionModel().getSelectedItem() == "SEARCH BY ID" && Integer.toString(item.getId()).equals(lowerCaseFilter)){
+                    return true;
+                }
+
+                if (itemSearchFilters.getSelectionModel().getSelectedItem() == "SEARCH BY DATE" && item.getDate().toString().contains(lowerCaseFilter)){
+                    return true;
+                }
+
+                if (itemSearchFilters.getSelectionModel().getSelectedItem() == "SEARCH BY SERIAL NUMBER" && item.getSerialNumber().toLowerCase().contains(lowerCaseFilter)){
+                    return true;
+                }
+
+                if (itemSearchFilters.getSelectionModel().getSelectedItem() == "SEARCH BY TYPE" && item.getType().toLowerCase().contains(lowerCaseFilter)){
+                    return true;
+                }
+
+                if (itemSearchFilters.getSelectionModel().getSelectedItem() == "SEARCH BY BRAND" && item.getBrand().toLowerCase().contains(lowerCaseFilter)){
+                    return true;
+                }
+
+                if (itemSearchFilters.getSelectionModel().getSelectedItem() == "SEARCH BY MODEL" && item.getModel().toLowerCase().contains(lowerCaseFilter)){
+                    return true;
+                }
+
+                if (itemSearchFilters.getSelectionModel().getSelectedItem() == "SEARCH BY CUSTOMER NAME" && item.getCustomerName().toLowerCase().contains(lowerCaseFilter)){
+                    return true;
+                }
+
+                if (itemSearchFilters.getSelectionModel().getSelectedItem() == "SEARCH BY STATUS" && item.getStatus().toLowerCase().contains(lowerCaseFilter)){
+                    return true;
+                }
+
+                if (itemSearchFilters.getSelectionModel().getSelectedItem() == "SEARCH BY PRICE" && Integer.toString(item.getPrice()).toLowerCase().contains(lowerCaseFilter)){
+                    return true;
+                }
+
+
+                return false;
+            });
+
+        });
+
+        SortedList<Item> sortedItems = new SortedList<>(filteredItems);
+
+        sortedItems.comparatorProperty().bind(itemTableView.comparatorProperty());
+
+        itemTableView.setItems(sortedItems);
+
+    }
+
     public TableView<Item> getItemTableView() {
         return itemTableView;
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        ObservableList<String> searchFilters = FXCollections.observableArrayList(
+            "SEARCH BY ID",
+                "SEARCH BY DATE",
+                "SEARCH BY SERIAL NUMBER",
+                "SEARCH BY TYPE",
+                "SEARCH BY BRAND",
+                "SEARCH BY MODEL",
+                "SEARCH BY CUSTOMER NAME",
+                "SEARCH BY STATUS",
+                "SEARCH BY PRICE"
+        );
+
+        itemSearchFilters.getItems().removeAll(itemSearchFilters.getItems());
+        itemSearchFilters.getItems().addAll(searchFilters);
+        itemSearchFilters.getSelectionModel().selectFirst();
+
         idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
         dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
         serialNumberColumn.setCellValueFactory(new PropertyValueFactory<>("serialNumber"));
